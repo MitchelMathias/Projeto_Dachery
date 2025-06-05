336c277181040f034d2d52209538da96c6665580
Mitchel Mathias <mitchelmathias2904@gmail.com>
2025-06-05 08:51:08 -0300

    teste de porta

--- dev/scripts/server.js
+++ dev/scripts/server.js
@@ -355,5 +355,5 @@ app.get('/ferias', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = 8080 || 3001;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
