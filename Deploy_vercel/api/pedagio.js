431b277ee434091cc7a2764d03675ae6017ea435
Mitchel Mathias <mitchelmathias2904@gmail.com>
2025-06-17 14:45:07 -0300

    detalhando o back-end pra tentar encontrar o ero

--- Deploy_vercel/api/pedagio.js
+++ Deploy_vercel/api/pedagio.js
@@ -5,16 +5,20 @@ let dado = 'Extração pendente';
 async function extrair() {
     let browser;
     try {
+        console.log('Iniciando browser...');
         browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
         const page = await browser.newPage();
+        console.log('Navegando para a página de login...');
         await page.setUserAgent('Mozilla/5.0');
-        await page.goto('https://auth.conectcar.com/auth/realms/Atacado/protocol/openid-connect/auth?client_id=portal-atacado-web&scope=openid%20email%20profile&response_type=code&redirect_uri=https%3A%2F%2Fcliente-frotas.conectcar.com%2Fapi%2Fauth%2Fcallback%2Fkeycloak&state=BYXLl_zClxVuXNnJtIap-hG9RMazGyC3Jb85z-_sWWk&code_challenge=cO4VVOMDg1QOztW3sFM_S3Qm2VHXIrKrHfk6OCVG0qI&code_challenge_method=S256', { waitUntil: 'networkidle2' });
+        await page.goto('https://auth.conectcar.com/auth/realms/Atacado/protocol/openid-connect/auth?...', { waitUntil: 'networkidle2' });
+        console.log('Preenchendo login...');
         await page.type('input[name="username"]', 'paulo@dachery.com.br');
         await page.type('input[name="password"]', 'Dachery@123');
         await Promise.all([
             page.click('button[type="submit"]'),
             page.waitForNavigation({ waitUntil: 'networkidle2' }),
         ]);
+        console.log('Acessando home...');
         await page.goto('https://cliente-frotas.conectcar.com/home', { waitUntil: 'networkidle2' });
         await page.waitForSelector('.font-bold.text-blue-4');
         dado = await page.$eval('.font-bold.text-blue-4', el => el.textContent.trim());
@@ -25,9 +29,11 @@ async function extrair() {
     } finally {
         if (browser) {
             await browser.close();
+            console.log('Browser fechado');
         }
     }
 }
+
 
 // Executa a extração a cada 15 minutos
 setInterval(extrair, 900000);
