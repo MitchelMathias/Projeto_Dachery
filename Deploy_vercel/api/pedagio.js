de2e5041a424a63bd176bd986e4b51ae134bcced
Mitchel Mathias <mitchelmathias2904@gmail.com>
2025-06-17 19:39:08 -0300

    atualizando dependências para usar puppeteer-core e chrome-aws-lambda

--- Deploy_vercel/api/pedagio.js
+++ Deploy_vercel/api/pedagio.js
@@ -1,4 +1,5 @@
-const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 
 async function extrair() {
     let browser = null;
@@ -6,17 +7,18 @@ async function extrair() {
 
     try {
         browser = await puppeteer.launch({
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath,
+            headless: chromium.headless,
         });
 
         const page = await browser.newPage();
         await page.setUserAgent('Mozilla/5.0');
 
-        await page.goto(
-            'https://auth.conectcar.com/auth/realms/Atacado/protocol/openid-connect/auth?client_id=portal-atacado-web&scope=openid%20email%20profile&response_type=code&redirect_uri=https%3A%2F%2Fcliente-frotas.conectcar.com%2Fapi%2Fauth%2Fcallback%2Fkeycloak&state=BYXLl_zClxVuXNnJtIap-hG9RMazGyC3Jb85z-_sWWk&code_challenge=cO4VVOMDg1QOztW3sFM_S3Qm2VHXIrKrHfk6OCVG0qI&code_challenge_method=S256',
-            { waitUntil: 'networkidle2' }
-        );
+        await page.goto('https://auth.conectcar.com/auth/realms/Atacado/protocol/openid-connect/auth?client_id=portal-atacado-web&scope=openid%20email%20profile&response_type=code&redirect_uri=https%3A%2F%2Fcliente-frotas.conectcar.com%2Fapi%2Fauth%2Fcallback%2Fkeycloak&state=BYXLl_zClxVuXNnJtIap-hG9RMazGyC3Jb85z-_sWWk&code_challenge=cO4VVOMDg1QOztW3sFM_S3Qm2VHXIrKrHfk6OCVG0qI&code_challenge_method=S256', {
+            waitUntil: 'networkidle2'
+        });
 
         await page.type('input[name="username"]', 'paulo@dachery.com.br');
         await page.type('input[name="password"]', 'Dachery@123');
@@ -28,16 +30,15 @@ async function extrair() {
 
         await page.goto('https://cliente-frotas.conectcar.com/home', { waitUntil: 'networkidle2' });
         await page.waitForSelector('.font-bold.text-blue-4', { timeout: 15000 });
-
         resultado = await page.$eval('.font-bold.text-blue-4', el => el.textContent.trim());
+
     } catch (err) {
         console.error('Erro ao extrair:', err);
         resultado = 'Erro na extração: ' + err.message;
     } finally {
         if (browser !== null) await browser.close();
     }
-    
-    console.log('Resultado da extração:', resultado);
+
     return resultado;
 }
 
