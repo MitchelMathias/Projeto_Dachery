904683bd54ce740f20b5ff536b066340650b4f04
Mitchel Mathias <mitchelmathias2904@gmail.com>
2025-06-17 19:14:12 -0300

    atualizando mensagem de erro na função de extração

--- Deploy_vercel/api/pedagio.js
+++ Deploy_vercel/api/pedagio.js
@@ -37,7 +37,7 @@ async function extrair() {
         console.log('Extração concluída:', dado);
     } catch (err) {
         console.error('Erro ao extrair:', err);
-        dado = 'Erro na extração';
+        dado = err;
         if (browser !== null) await browser.close();
     }
 }
