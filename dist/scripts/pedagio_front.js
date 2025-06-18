46c65de6a663ba2b3bb80ac43a3362db789aaad6
Mitchel Mathias <mitchelmathias2904@gmail.com>
2025-06-18 10:58:01 -0300

    atualizando o intervalo de execução da função de extração para 50 segundos e adicionando log da resposta

--- dist/scripts/pedagio_front.js
+++ dist/scripts/pedagio_front.js
@@ -2,9 +2,10 @@ async function fetchDado() {
     try {
         const res = await fetch('/dado');
         document.getElementById('resultado').innerText = await res.text();
+        console.log(res.text())
     } catch {
         document.getElementById('resultado').innerText = 'Erro ao buscar';
     }
 }
 fetchDado();
-setInterval(fetchDado, 5000);
+setInterval(fetchDado, 50000);
