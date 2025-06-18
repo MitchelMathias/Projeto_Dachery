0b74da8686cca0c9f57cdf16664c2ebfe66d43cf
Mitchel Mathias <mitchelmathias2904@gmail.com>
2025-06-18 13:06:09 -0300

    Corrigir atribuição de erro na extração de dados para usar método correto

--- Deploy_vercel/api/pedagio.js
+++ Deploy_vercel/api/pedagio.js
@@ -26,7 +26,7 @@ module.exports = async (req,res) => {
             await browser.close();
         } catch (err) {
             console.error('Erro na extração:', err);
-            dado = err
+            dado = err.text()
         }
     }
     await extrair();
